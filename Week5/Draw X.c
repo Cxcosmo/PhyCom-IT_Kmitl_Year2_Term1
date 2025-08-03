12bb1a57cab22297b19800f10f49c6e7d887b511
/ᐠ. ｡.ᐟ\ᵐᵉᵒʷˎˊ˗ <thanapon9114@gmail.com>
2025-08-03 23:48:09 +0700

    eiei

--- Week5/Draw X.c
+++ Week5/Draw X.c
@@ -3,11 +3,11 @@
 int main(){
     int n;
 
-    scanf("%d / 2", &n);
+    scanf(" %d", &n);
     n /= 2;
     for (int i = -n; i <= n; i++){
         for (int j = -n; j <= n; j++){
-            i == j || i == 0 - j ? printf("-") : printf("#");
+            i == j || i == -j ? printf("-") : printf("#");
         }
         printf("\n");
     }
