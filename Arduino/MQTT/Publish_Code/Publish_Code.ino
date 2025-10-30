2f677e5504a347ef5f2d3c440008b28b076be810
/ᐠ. ｡.ᐟ\ᵐᵉᵒʷˎˊ˗ <thanapon9114@gmail.com>
2025-10-30 23:22:31 +0700

    add : MQTT

---
+++ Arduino/MQTT/Publish_Code/Publish_Code.ino
@@ -0,0 +1,84 @@
+/*
+ *
+ * This Arduino UNO R4 code is made available for public use without any restriction
+ *
+ */
+
+#include <WiFiS3.h>
+#include <MQTTClient.h>
+
+const char WIFI_SSID[] = "Net for cat";     // CHANGE TO YOUR WIFI SSID
+const char WIFI_PASSWORD[] = "Cat_Only";  // CHANGE TO YOUR WIFI PASSWORD
+
+const char MQTT_BROKER_ADRRESS[] = "phycom.it.kmitl.ac.th";  // CHANGE TO MQTT BROKER'S ADDRESS
+const int MQTT_PORT = 1883;
+const char MQTT_CLIENT_ID[] = "67070066";  // CHANGE IT AS YOU DESIRE
+const char MQTT_USERNAME[] = "";              // CHANGE IT IF REQUIRED, empty if not required
+const char MQTT_PASSWORD[] = "";              // CHANGE IT IF REQUIRED, empty if not required
+
+// The MQTT topics that Arduino should publish/subscribe
+const char PUBLISH_TOPIC[] = "67070066/topic";       // CHANGE IT AS YOU DESIRE
+
+const int PUBLISH_INTERVAL = 5000;  // 5 seconds
+
+WiFiClient network;
+MQTTClient mqtt = MQTTClient(256);
+
+unsigned long lastPublishTime = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  int status = WL_IDLE_STATUS;
+  while (status != WL_CONNECTED) {
+    Serial.print("Arduino UNO R4 - Attempting to connect to SSID: ");
+    Serial.println(WIFI_SSID);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  // print your board's IP address:
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  connectToMQTT();
+}
+
+void loop() {
+  mqtt.loop();
+
+  if (millis() - lastPublishTime > PUBLISH_INTERVAL) {
+    sendToMQTT();
+    lastPublishTime = millis();
+  }
+}
+
+void connectToMQTT() {
+  // Connect to the MQTT broker
+  mqtt.begin(MQTT_BROKER_ADRRESS, MQTT_PORT, network);
+
+  Serial.print("Arduino UNO R4 - Connecting to MQTT broker");
+
+  while (!mqtt.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD)) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+
+  if (!mqtt.connected()) {
+    Serial.println("Arduino UNO R4 - MQTT broker Timeout!");
+    return;
+  }
+
+  Serial.println("Arduino UNO R4 - MQTT broker Connected!");
+}
+
+void sendToMQTT() {
+  // mqtt.publish(PUBLISH_TOPIC, "String");  //publish to mqtt
+
+  //code here
+
+  delay(2000);
+}
